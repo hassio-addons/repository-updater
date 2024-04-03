d4c2e805237d46ce66ecb1bce2ad80f326bfd818
Daniel Gangl <31815106+killer0071234@users.noreply.github.com>
2024-04-03 16:47:59 +0000

    Try to add git name and email

--- action/entrypoint.sh
+++ action/entrypoint.sh
@@ -15,6 +15,14 @@ fi
 [[ "${INPUT_FORCE,,}" = "true" ]] \
   && options+=(--force)
 
+if [[ -n "${GITHUB_ACTOR:-}" ]]; then
+  options+=(--git-name "${GITHUB_ACTOR}")
+  git config --global user.name "${GITHUB_ACTOR}"
+
+if [[ -n "${GITHUB_ACTOR_ID:-}" ]]; then
+  options+=(--git-email "${GITHUB_ACTOR_ID}+${GITHUB_ACTOR}@users.noreply.github.com")
+  git config --global user.email "${GITHUB_ACTOR_ID}+${GITHUB_ACTOR}@users.noreply.github.com"
+
 # Output version
 repository-updater --version
 
